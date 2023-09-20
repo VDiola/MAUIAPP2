50861017a5f27c26f59cfb3702eced950708ea5f
VDiola <126273351+VDiola@users.noreply.github.com>
2023-09-20 08:23:57 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ImgBtn1_Clicked(object sender, EventArgs e)
+        {
+            
+            imgBtn1.Source = "img3.png";
+            lbl1.Text = "Reality";
+        }
+
+        private void ImgBtn2_Clicked(object sender, EventArgs e)
+        {
+            
+            imgBtn2.Source = "img4.png";
+            lbl2.Text = "Reality";
+        }
+
+    }
+}
